802619edd470fa5f81fb9b6bbde6f2c90e92ebfe
罗智毅 <zhiyiluo@drgou.com>
2020-05-08 18:05:33 +0800

    版本1.0.10

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Icon } from './Icon';
 export { default as notification } from './Notification';
 export { default as message } from './Message';
+export { default as Modal } from './Modal';
